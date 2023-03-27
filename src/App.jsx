7c13fbd5649c3c689946852968199900dd3eaad1
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-03-27 10:12:12 +0600

    added cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import './App.css'
+import Shop from './components/Shop/Shop';
 
 const App = () => {
   return (
     <div>
       <Header></Header>
-      
+      <Shop></Shop>
     </div>
   );
 };
